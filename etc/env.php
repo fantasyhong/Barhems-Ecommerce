74495514aae75ceaf7a5969f80f111451e5f4abc
Tony Hong <tonyhong63@gmail.com>
2018-05-03 12:58:03 -0400

    First commit

---
+++ etc/env.php
@@ -0,0 +1,49 @@
+<?php
+return [
+  'backend' => [
+    'frontName' => 'admin'
+  ],
+  'crypt' => [
+    'key' => '42cb9b8481788b20f2b2949341d0cece'
+  ],
+  'db' => [
+    'table_prefix' => '',
+    'connection' => [
+      'default' => [
+        'host' => 'localhost',
+        'dbname' => 'magento2',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1'
+      ]
+    ]
+  ],
+  'resource' => [
+    'default_setup' => [
+      'connection' => 'default'
+    ]
+  ],
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'session' => [
+    'save' => 'files'
+  ],
+  'cache_types' => [
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1
+  ],
+  'install' => [
+    'date' => 'Thu, 03 May 2018 14:53:48 +0000'
+  ]
+];
